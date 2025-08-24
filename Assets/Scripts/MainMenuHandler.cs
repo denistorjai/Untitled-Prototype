d81374769b27aee68d17385e3313dd430db8fdb6
Denis Torjai <denistorjai@outlook.com>
2025-08-24 16:42:08 -0600

    Added Settings Tab

--- Assets/Scripts/MainMenuHandler.cs
+++ Assets/Scripts/MainMenuHandler.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 public class MainMenuHandler : MonoBehaviour
 {
 
+    public GameObject MenuCanvas;
+    public GameObject SettingsCanvas;
+    
     public void StartGame()
     {
         GameManager.instance.StartGame();
@@ -11,6 +14,22 @@ public class MainMenuHandler : MonoBehaviour
     public void CloseGame()
     {
         Application.Quit();
+    }
+
+    public string CurrentMenu = "Main";
+
+    public void OpenSettings()
+    {
+        CurrentMenu = "Settings";
+        MenuCanvas.SetActive(false);
+        SettingsCanvas.SetActive(true);
+    }
+
+    public void BackToMainMenu()
+    {
+        CurrentMenu = "MainMenu";
+        MenuCanvas.SetActive(true);
+        SettingsCanvas.SetActive(false);
     }
     
 }
